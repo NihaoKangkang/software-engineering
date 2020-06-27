e6394a2c6630f74a6bab3507fa355a101e9a5ea3
wangshuoXuniji <kyiwong97@gmail.com>
2020-06-28 00:38:06 +0800

    mysql fixed

--- mysql/database.sql
+++ mysql/database.sql
@@ -56,5 +56,5 @@ insert into car (car_ID, car_v, car_driver, car_phone) values ('GA10004', 5, 'DD
 insert into car (car_ID, car_v, car_driver, car_phone) values ('GA10005', 60, 'EEE', '13666667770');
 insert into clock (car_ID, begin_time, begin_place) values ('GA10001', '2020/5/20  19:55:32', 'Aplace');
 insert into clock (car_ID, begin_time, begin_place) values ('GA10004', '2020/5/22  01:05:32', 'Bplace');
-insert into book (car_ID, stu_ID) values ('GA10001', '132112354610');
-insert into book (car_ID, stu_ID) values ('GA10004', '132112354727');
+insert into book (car_ID, stu_ID, begin_time) values ('GA10001', '132112354610','2020/5/20  19:55:32');
+insert into book (car_ID, stu_ID, begin_time) values ('GA10004', '132112354727','2020/5/22  01:05:32');
